6fb63e7daa8dc72b82ea888f04414550b69a453c
Tim <nigh.timothy@gmail.com>
2023-03-18 20:10:48 -0400

    Added 404 pages and an index

---
+++ app.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const http = require('http');
+const PORT = 8080;
+
+const httpServer = http.createServer(httpHandler);
+
+httpServer.listen(PORT, "",()=>{
+    console.log(`Listening on port ${PORT}`);
+});
+
+function httpHandler(request, response){
+    console.log(` Request from ${request.ip} for ${request.url}`);
+    
+    if(request.url === '/'){
+        request.url = 'index.html';
+    }
+
+    fs.readFile('./pub/' + request.url, (err,data) =>{
+        if(err == null){
+            response.writeHead(200, {'Content-Type':'text/html'})
+            response.write(data);
+            response.end()
+        }
+        else{
+            console.log("returning 404");
+            fs.readFile('./priv/404.html', (err, data) =>{
+                if(err == null){
+                    response.writeHead(404, {'Content-Type':'text/html'})
+                    response.write(data);
+                    response.end()
+                }
+            })
+        }
+    })
+}
+
